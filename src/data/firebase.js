a2a9e851c323546122f3bca1a3cb13223b7ba864
JeremyEmondLapierre <emondlapierrejeremy@gmail.com>
2021-03-31 18:59:46 -0400

    Dépôt final de l'exercice 2

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,12 +4,7 @@ import firestore from 'firebase/firestore';
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAycOwAdmCS-RBnyoE3q4p7YSDfXgvFWa0",
-  authDomain: "react-iwra-4ccd3.firebaseapp.com",
-  projectId: "react-iwra-4ccd3",
-  storageBucket: "react-iwra-4ccd3.appspot.com",
-  messagingSenderId: "619578293854",
-  appId: "1:619578293854:web:9b6a0596b3fac83b71fecf"
+  
 };
 
 // Initialiser Firebase
